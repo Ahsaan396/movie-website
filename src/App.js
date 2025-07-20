41dbb666c3ae859b0cd959935d985d37719b624f
Ahsaan396 <ahsanulhakim285@gmail.com>
2025-07-20 14:52:17 +0600

    I've added to pages called Home and Favorite. For dynamically rendering movies I used Map() function. I used a Search Form() to search movie. In this form I used State for rendering the page without reloading the whole page. Example: const[searchQuery, setSearchQuery] = useState(). Now, searchQuery is a function name whereas setSearchQuery is a function.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 
 import './App.css';
-import MovieCard from './components/MovieCard';
-
+//import MovieCard from './components/MovieCard';
+import Home from './pages/Home';
 function App() {
   return (
     <div>
-      <MovieCard movie={{
-        title:'Titanic',
-        released_date:'2024'
-      }}/>
-      <MovieCard movie={{
-        title:'John wick',
-        released_date:'2011'
-      }}/>
-      <MovieCard movie={{
-        title:'GOT',
-        released_date:'2024'
-      }}/>
+      <Home />
     </div>
   );
 }
 
-function Text({display}){
-  return (
-    <div>
-      <p>{display}</p>
-    </div>
-  );
-}
+
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+    const movies = [
+        { id: 1, title: "John Wick", released_date: "2025" },
+        { id: 2, title: "Game of Thrones", released_date: "2010" },
+        { id: 3, title: "Titanic", released_date: "2020" }
+    ];
+    const [searchQuery, setSearchQuery]=useState("");
+    const handleSearch= () =>{};
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" 
+                placeholder="Search for movies ..." 
+                className="search-input"
+                value={searchQuery}
+                onChange={(e)=>setSearchQuery(e.target.value)}
+                
+                />
+                <button type="submit" className="search-btn">Search</button>
+            </form>
+            <div className="movies-grid">
+                {movies.map((movie) => (
+                    movie.title.toLowerCase().startsWith(searchQuery) && <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>
+        </div>
+    );
+
+}
+export default Home
